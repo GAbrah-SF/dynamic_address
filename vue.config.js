63f9ce68a2b938ff7918c2040c1d2b36daa3ffed
GAbrah-SF <ga.sanzfons@gmail.com>
2021-10-25 17:41:30 -0500

    Mi primera app de Vue.js-3

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//       ? '/dynamic_address/'
-//       : '/'
-// }
-
-module.exports = {
-  publicPath: '/dynamic_address/'
-}
